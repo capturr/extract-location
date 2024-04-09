474b682e5e74c109d84b751ab502d589b68579f9
gaetanlegac <contact@gaetan-legac.fr>
2024-04-09 18:31:26 +0200

    Fix & improve tests

--- test.js
+++ test.js
@@ -4,28 +4,32 @@ const extractLocation = require('./bin/index.js').default;
 test('Extract from a simple city name', () => {
     expect( extractLocation('New York') ).toStrictEqual({
         country: 'United States',
-        city: 'New York'
+        city: 'New York',
+        reliability: 2
     });
 });
 
 test('Extract from a city name with noise', () => {
     expect( extractLocation('Location: Paris (16)') ).toStrictEqual({
         country: 'France',
-        city: 'Paris'
+        city: 'Paris',
+        reliability: 2
     });
 });
 
 test('Extract from a city name and country name', () => {
     expect( extractLocation('Paris, United States') ).toStrictEqual({
         country: 'United States',
-        city: 'Paris'
+        city: 'Paris',
+        reliability: 3
     });
 });
 
 test('Extract from a city name and precise the country', () => {
     expect( extractLocation('Paris, United States', 'Canada') ).toStrictEqual({
         country: 'Canada',
-        city: 'Paris'
+        city: 'Paris',
+        reliability: 3
     });
 });
 
@@ -39,4 +43,12 @@ test('Is insnsitive to dashes in name', () => {
 
 test('No location provided', () => {
     expect( extractLocation('There is no location here') ).toStrictEqual( undefined );
+});
+
+test('Word boundaries are well detected', () => {
+    expect( extractLocation('92300 Levallois-Perret•Télétravail partiel') ).toStrictEqual({
+        country: 'France',
+        city: 'Levallois-Perret',
+        reliability: 2
+    });
 });
